9c8473633ea5ddd3da72a1d5ed5053120f94a227
Jun Kajii <junk4jii@gmail.com>
2022-08-05 15:16:43 -0300

    :recycle: refactor hud

--- HUD.gd
+++ HUD.gd
@@ -3,6 +3,10 @@ extends CanvasLayer
 signal start_game
 
 
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+	
 func show_message(text):
 	$MessageLabel.text = text
 	$MessageLabel.show()
@@ -12,14 +16,10 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	yield($MessageTimer, "timeout")
-	$MessageLabel.text = "Dodge the\nCreeps!"
+	$MessageLabel.text = "Dodge\nthe\nCreeps!"
 	$MessageLabel.show()
 	yield(get_tree().create_timer(1), "timeout")
 	$StartButton.show()
-
-
-func update_score(score):
-	$ScoreLabel.text = str(score)
 
 
 func _on_StartButton_pressed():
